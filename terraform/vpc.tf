54acb15bd1338d814eb49530f1ffb120d153e4e5
rtaro02 <nmeeaiwxntwsoetnlelin@gmail.com>
2021-10-09 12:27:27 +0900

    update

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -9,24 +9,41 @@ resource "google_compute_subnetwork" "this" {
   ip_cidr_range = "10.240.0.0/24"
   region        = "us-west1"
   network       = google_compute_network.this.id
-  project = google_project.this.name
+  project       = google_project.this.name
 }
 
 resource "google_compute_firewall" "kubernetes-the-hard-way-allow-internal" {
   name    = "kubernetes-the-hard-way-allow-internal"
   network = google_compute_network.this.name
-
+  project = google_project.this.name
   allow {
     protocol = "tcp"
   }
-
   allow {
     protocol = "udp"
   }
-
   allow {
     protocol = "icmp"
-  }     
+  }
+  source_ranges = ["10.240.0.0/24", "10.200.0.0/16"]
+}
 
-  source_tags = ["web"]
+resource "google_compute_firewall" "kubernetes-the-hard-way-allow-external" {
+  name    = "kubernetes-the-hard-way-allow-external"
+  network = google_compute_network.this.name
+  project = google_project.this.name
+  allow {
+    protocol = "tcp"
+    ports    = ["22", "6443"]
+  }
+  allow {
+    protocol = "icmp"
+  }
+  source_ranges = ["0.0.0.0/0"]
+}
+
+resource "google_compute_address" "default" {
+  name    = "kubernetes-the-hard-way"
+  region  = "us-west1"
+  project = google_project.this.name
 }
